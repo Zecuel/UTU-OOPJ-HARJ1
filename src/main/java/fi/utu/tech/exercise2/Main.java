81ad2eba9b15b1677a9f9b8a06c66698a4c3d922
Zecuel <lauri-mikael.pakkanen@harriot.fi>
2021-09-10 13:15:57 +0300

    Added example call to printBoard() (exercise 2)

--- src/main/java/fi/utu/tech/exercise2/Main.java
+++ src/main/java/fi/utu/tech/exercise2/Main.java
@@ -4,6 +4,8 @@ import fi.utu.tech.exercise2.task1.Utilities;
 import fi.utu.tech.exercise2.task3.Age;
 import fi.utu.tech.exercise2.task4.Foobar;
 
+import fi.utu.tech.exercise2.task2.ChessBoard;
+
 public class Main {
     /**
      * Main class.
@@ -11,6 +13,9 @@ public class Main {
      * @param args Command line arguments
      */
     public static void main(String[] args) {
+
+        ChessBoard.printBoard(ChessBoard.createEmptyBoard(), true);
+
         for (var i : Foobar.foobar(new int[]{5, -6, 6, 6, 2, 3, 3, 3, 1})) {
             System.out.println(i);
         }
